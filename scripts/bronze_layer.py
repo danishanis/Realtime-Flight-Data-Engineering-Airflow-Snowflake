19e8c3ad3b039530018ab23fa0d1a6b62a85ae23
Danish Anis <51137481+danishanis@users.noreply.github.com>
2026-04-09 15:25:47 -0400

    Comments added for accurate data flow in bronze layer

--- scripts/bronze_layer.py
+++ scripts/bronze_layer.py
@@ -6,15 +6,28 @@ import json
 from datetime import datetime
 from pathlib import Path
 
-# Current state of all aircrafts being tracked globally by OpenSky Network API
+# Source endpoint: Current state of all aircrafts being tracked globally by OpenSky Network API
 URL = "https://opensky-network.org/api/states/all"
 
 def run_bronze_ingestion(**context):
-    response = requests.get(URL, timeout=30)
-    response.raise_for_status()
+    """
+    Function running within Airflow to fetch flight data from OpenSky API
+
+    Input Parameters:
+        - context:  Airflow's task context dictionary — it gets automatically injected 
+                    when Airflow runs this function via a PythonOperator It carries metadata 
+                    about the current DAG run, execution time, task instance etc. 
+                    
+                    Without it, you can't use context["ti"] to push an XCom value at the end.
+    """
+    response = requests.get(URL, timeout=30) # API call
+    response.raise_for_status() # Check if the request was successful, if not it will raise an HTTPError
 
     data = response.json()
 
+    # Every API call produces a uniquely named file based on the current timestamp. Re-running the pipeline
+    # will create a new file without overwriting previous data, allowing us to maintain a historical record 
+    # of all ingested flight data.
     timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
 
     path = Path(f"/opt/airflow/data/bronze/flights_{timestamp}.json")
@@ -24,4 +37,6 @@ def run_bronze_ingestion(**context):
     with open(path, "w") as f:
         json.dump(data, f)
 
+    # 'ti' = Task Instance, used by Airflow tasks to pass data b/w each other
+    # The downstream silver task will call the xcom_pull to know which bronze file was created to be processed
     context["ti"].xcom_push(key="bronze_file", value=str(path))
